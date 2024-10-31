65c708a7806db3ebb117a2577ae433dbdc48204f
BiMeow <trungnguyen@immi-go.com>
2024-10-31 18:43:08 +0700

    fix main intro image + animation

--- src/components/sections/home/IntroStep1.tsx
+++ src/components/sections/home/IntroStep1.tsx
@@ -6,17 +6,22 @@ function IntroStep1({ ...props }) {
   const { setIntroStep } = useStorage();
   return (
     <>
-      <motion.div exit={{ x: -400, opacity: 0 }} className={`IntroStep1`}>
+      <motion.div
+        exit={{ x: -400, opacity: 0 }}
+        className={`IntroStep1 px-[30px]`}
+      >
         <img
           src="/images/intro-text.png"
           alt="Tiki's Quiz Intro Text"
-          className="introTitle w-full max-h-[27vh] object-contain mb-[1.1vh] opacity-0"
+          className="introTitle w-full max-h-[30vh] object-contain mb-[1.1vh] opacity-0"
         />
-        <p className="introDesc text-[2vh] font-medium text-center leading-[1.1] mb-[3vh] opacity-0">
-          Mẹ là kiểu nào? <br /> Cùng TIKI khám phá nhé!
+        <p className="introDesc text-[2.4vh] font-medium text-center leading-[1.1] mb-[3vh] opacity-0">
+          Bạn có tò mò mình là kiểu mẹ nào?
+          <br />
+          Cùng Tiki khám phá nhé!
         </p>
         <div
-          className="introBtn mainBtn mx-auto !text-[2vh] opacity-0"
+          className="introBtn mainBtn mx-auto !text-[2.4vh] opacity-0"
           onClick={() => {
             setIntroStep(0);
             setTimeout(() => {
